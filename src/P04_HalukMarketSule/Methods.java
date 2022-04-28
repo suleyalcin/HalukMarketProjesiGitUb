0cdd5c250ebf3249db2816f17951a86d4328232f
Sevgi Akar <sesamu23@gmail.com>
2022-04-28 18:28:16 +0300

    Sevgi

--- src/P04_HalukMarketSule/Methods.java
+++ src/P04_HalukMarketSule/Methods.java
@@ -1,5 +1,26 @@
 package P04_HalukMarketSule;
 
-public class Methods {
-    //sevgi
+import java.util.List;
+import java.util.Scanner;
+
+public class Methods extends Variables {
+    public static void urunSec(List<String> urunListesi, List<Double>fiyatListesi){
+        System.out.println("******** HALUK MARKETE HOSGELDİNİZ **********");
+        System.out.println("Lütfen almak istediginiz urunun numarasını giriniz :\n " +urunListesi);
+        int secilenUrun=scan.nextInt();
+        System.out.println("Lütfen kac kg almak istediginizi giriniz : ");
+        double miktar=scan.nextDouble();
+        sepet.add(miktar+ " kg " + (urunListesi.get(secilenUrun - 1 ).replaceAll("\\d","")))+
+                sepetTutari += fiyatListesi.get(secilenUrun -1)*miktar;
+        System.out.println("Su an sepetteki urunler : " +sepet);
+        secimYapMetodu();
+
+
+    }
+
+    private static void secimYapMetodu() {
+
+    }
+
+
 }
