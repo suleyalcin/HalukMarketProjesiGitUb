69d2210c372cf06f2bd39ac9762a23a6a6aa124b
cemiloluc <alliswell1903@gmail.com>
2022-04-28 22:21:18 +0300

    ucuncu

--- src/P04_HalukMarketSule/HalukMarket.java
+++ src/P04_HalukMarketSule/HalukMarket.java
@@ -17,5 +17,23 @@ public class HalukMarket {
         }
 
 
+        secimYapMethodu();
+    }
+
+    public static void secimYapMethodu() {
+
+        System.out.println("Alışverişe devam etmek istiyorsanız E tuşuna, istemiyorsanız H tuşuna basınız.");
+        Scanner scan =new Scanner(System.in);
+        String secim = scan.next().substring(0,1).toUpperCase(Locale.ROOT);
+
+        if (secim.equals("E")){
+            // urunsec(urunListesi, fiyatListesi);
+        }else if (secim.equals("H")){
+            //  paraUstuHesaplaMethodu();
+        }else {
+            System.out.println("Hatali secim yaptiniz. Lutfen tekrar deneyiniz.");
+            secimYapMethodu();
+        }
+
     }
 }
--- src/P04_HalukMarketSule/Methods.java
+++ src/P04_HalukMarketSule/Methods.java
@@ -1,4 +1,11 @@
 package P04_HalukMarketSule;
 
+import static P04_HalukMarketSule.HalukMarket.secimYapMethodu;
+
 public class Methods {
+    public static void main(String[] args) {
+        HalukMarket getir = new HalukMarket();
+        secimYapMethodu();
+    }
+
 }
