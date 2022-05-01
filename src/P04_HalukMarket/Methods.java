adb3ec2db7c6a0361438a35cfb393ab0414bf61e
cemiloluc <alliswell1903@gmail.com>
2022-05-01 13:36:36 +0300

    cemil..

--- src/P04_HalukMarket/Methods.java
+++ src/P04_HalukMarket/Methods.java
@@ -1,6 +1,23 @@
 package P04_HalukMarket;
-
+import java.util.Locale;
+import java.util.Scanner;
 public class Methods extends Variables{
 
 
+    public static void secimYapMethodu() {
+
+        System.out.println("Alışverişe devam etmek istiyorsanız E tuşuna, istemiyorsanız H tuşuna basınız.");
+        Scanner scan = new Scanner(System.in);
+        String secim = scan.next().substring(0, 1).toUpperCase(Locale.ROOT);
+
+        if (secim.equals("E")) {
+            // urunsec(urunListesi, fiyatListesi);
+        } else if (secim.equals("H")) {
+            //  paraUstuHesaplaMethodu();
+        } else {
+            System.out.println("Hatali secim yaptiniz. Lutfen tekrar deneyiniz.");
+            secimYapMethodu();
+            //
+        }
+    }
 }
