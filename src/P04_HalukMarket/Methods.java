3f186ed650774598c590e5deb8ab422f85320233
cemiloluc <alliswell1903@gmail.com>
2022-05-04 20:37:19 +0300

    4 mayıs 20.37

--- src/P04_HalukMarket/Methods.java
+++ src/P04_HalukMarket/Methods.java
@@ -8,11 +8,14 @@ import java.util.Scanner;
 
 public class Methods extends Variables {
 
-    public static double urunSec(List<String> urunListesi, List<Double> fiyatListesi) {
+    public static <max> double urunSec(List<String> urunListesi, List<Double> fiyatListesi) {
         System.out.println("******** HALUK MARKETE HOSGELDİNİZ **********");
 
 
         System.out.println("Lütfen almak istediginiz urunun numarasını giriniz :\n " + urunListesi);
+
+
+
         int secilenUrun = 0;
 
         while (true) {
@@ -26,20 +29,32 @@ public class Methods extends Variables {
         }
 
 
+
+        if (secilenUrun>12){
+            System.out.println("Lütfen aşağıda bulunan listeden bir ürün numarası giriniz");
+            urunSec(urunListesi,fiyatListesi);
+        }else if (secilenUrun<1){
+            System.out.println("Lütfen aşağıda bulunan listeden bir ürün numarası giriniz");
+            urunSec(urunListesi,fiyatListesi);
+        }else
+
+
+
+
         System.out.println("lütfen kac kg almak istediginizi giriniz : ");
         double miktar = 0;
 
-        while (true) {
-            try {
-                miktar = scan.nextDouble();
-                break;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("lütfen miktar giriniz");
-                scan.nextLine();
-                miktar = scan.nextDouble();
-            }
-            return miktar;
+    while (true) {
+        try {
+            miktar = scan.nextInt();
+            break;
+        } catch (InputMismatchException e) {
+            System.out.println("lütfen miktar giriniz");
+            scan.nextLine();
         }
+    }
+
+
 
         sepet.add(miktar + " kg " + (urunListesi.get(secilenUrun - 1).replaceAll("\\d", "")) + " ");
         sepetTutari += fiyatListesi.get(secilenUrun - 1) * miktar;
@@ -56,6 +71,8 @@ public class Methods extends Variables {
         System.out.println("Lütfen hesap bakiyenizi giriniz : ");
         musteriBakiye = scan.nextDouble();
         System.out.println("Hesap bakiyeniz : " + musteriBakiye + " tl ödemeniz gereken tutar :" + (double) sepetTutari + "tl");
+
+
         if (musteriBakiye >= sepetTutari) {
             System.out.println("Para üstünüz : " + (double) (musteriBakiye - sepetTutari) + "tldir \n" + " ***Yine Bekleriz***");
             return (musteriBakiye - sepetTutari);
@@ -67,11 +84,15 @@ public class Methods extends Variables {
         return 0;
     }
 
+
+
+
     public static void secimYapMethodu() {
 
         System.out.println("Alışverişe devam etmek istiyorsanız E tuşuna, istemiyorsanız H tuşuna basınız.");
         Scanner scan = new Scanner(System.in);
         String secim = scan.next().substring(0, 1).toUpperCase(Locale.ROOT);
+
 
         if (secim.equals("E")) {
             urunSec(urunListesi, fiyatListesi);
